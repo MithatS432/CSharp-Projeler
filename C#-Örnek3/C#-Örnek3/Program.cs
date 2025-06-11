d25ae7d71db16a7e255a646bff1766ba528bb364
Mithat <mithatsirmali@gmail.com>
2025-06-11 18:40:32 +0300

    Add files via upload

---
+++ C#-Örnek3/C#-Örnek3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Örnek3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir kelime girin: ");
+            string kelime = Console.ReadLine().ToLower();
+            string ters = "";
+
+            for (int i = kelime.Length - 1; i >= 0; i--)
+            {
+                ters += kelime[i];
+            }
+
+            if (kelime == ters)
+                Console.WriteLine("Palindromdur.");
+            else
+                Console.WriteLine("Palindrom değildir.");
+        }
+    }
+}
