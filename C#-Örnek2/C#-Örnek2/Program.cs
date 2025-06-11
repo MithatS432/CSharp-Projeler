8b1096efba6cdc78ba2b0753ec5324f9649a5506
Mithat <mithatsirmali@gmail.com>
2025-06-11 18:40:11 +0300

    Add files via upload

---
+++ C#-Örnek2/C#-Örnek2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Örnek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 3, 5, 7, 2, 9 };
+            int toplam = 0;
+
+            foreach (int sayi in sayilar)
+            {
+                toplam += sayi;
+            }
+
+            Console.WriteLine("Toplam: " + toplam);
+        }
+    }
+}
