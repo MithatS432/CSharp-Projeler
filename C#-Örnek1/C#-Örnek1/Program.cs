3c3b64e95dbbf8195c256302b61778e3aa6313b9
Mithat <mithatsirmali@gmail.com>
2025-06-11 18:39:55 +0300

    Add files via upload

---
+++ C#-Örnek1/C#-Örnek1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Örnek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı girin: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi % 2 == 0)
+                Console.WriteLine("Sayı çifttir.");
+            else
+                Console.WriteLine("Sayı tektir.");
+        }
+    }
+}
